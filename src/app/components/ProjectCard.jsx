bcf616d761923d056825333b5a56818eed0ea597
DiegoHS07 <dhiguera09@gmail.com>
2023-10-02 13:52:56 -0500

    Actualice el estilo de la tarjeta del proyecto y agregue el botón Ver en GitHub

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -44,9 +44,10 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl, skills })
                 </div>
             </div>
             <div className="overlay rounded-xl items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-                <Link href={gitUrl} target='blank' className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link flex flex-row'>
-                    <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white'/>
-                    Ver en GitHub   
+                <Link href={gitUrl} target='blank' className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white' title='Ver en GitHub'>
+                    <CodeBracketIcon className='h-10 w-10 text-[#88898a] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer hover:text-white'>
+                        asd
+                    </CodeBracketIcon>
                 </Link>
             </div>
         
