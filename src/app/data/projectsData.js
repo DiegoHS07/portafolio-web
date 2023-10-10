8cdcf17f94424f34b5f973d4150702bf65036cbd
DiegoHS07 <dhiguera09@gmail.com>
2023-10-10 15:08:52 -0500

    Actualización listado proyectos

--- src/app/data/projectsData.js
+++ src/app/data/projectsData.js
@@ -1,118 +1,151 @@
 export const projectsData = [
-    {
-        id: 1,
-        title: "Portafolio web con NextJs",
-        description: "Portafolio virtual donde puedo mostrar mi cv de forma innovadora",
-        image: "/images/projects/Portafolio-web.png",
-        tag: ["Todos", "Web"],
-        gitUrl: "https://github.com/DiegoHS07/portafolio-web",
-        skills:[
-            {
-                name: "GIT",
-                icon: "git.png",
-            },
-            {
-                name: "HTML",
-                icon: "html.png",
-            },
-            {
-                name: "CSS3",
-                icon: "css.png",
-            },
-            {
-                name: "JAVASCRIPT",
-                icon: "javascript.png",
-            },
-            {
-                name: "REACT",
-                icon: "react.png",
-            },
-            {
-                name: "TAILWIND",
-                icon: "tailwind.png",
-            },
-        ],
-    },
-    {
-        id: 2,
-        title: "Pokedex con Lit-Element",
-        description: "Creación de pokedex demostrando mis habilidades al implementar API's",
-        image: "/images/projects/Pokedex-LitElement.png",
-        tag: ["Todos", "Web"],
-        gitUrl: "https://github.com/DiegoHS07/Pokedex-LitElement",
-        skills:[
-            {
-                name: "GIT",
-                icon: "git.png",
-            },
-            {
-                name: "HTML",
-                icon: "html.png",
-            },
-            {
-                name: "CSS3",
-                icon: "css.png",
-            },
-            {
-                name: "JAVASCRIPT",
-                icon: "javascript.png",
-            },
-            {
-                name: "LITELEMENT",
-                icon: "litelement.png",
-            },
-            {
-                name: "NPM",
-                icon: "npm.png",
-            },
-        ],
-    },
-    {
-        id: 3,
-        title: "SAAM",
-        description: "Creación proyecto fin de grado y presentado en ACOFI 2020 y 2021",
-        image: "/images/projects/SAAM.png",
-        tag: ["Todos", "Movil"],
-        gitUrl: "https://github.com/DiegoHS07/UCC_SAAM",
-        skills:[
-            {
-                name: "GIT",
-                icon: "git.png",
-            },
-            {
-                name: "DART",
-                icon: "dart.png",
-            },
-            {
-                name: "FLUTTER",
-                icon: "flutter.png",
-            },
-            {
-                name: "FIREBASE",
-                icon: "firebase.png",
-            },
-        ],
-    },
-    {
-        id: 4,
-        title: "Gladiadores APP",
-        description: "Creación videojuego RPG temática de gladiadores para Android",
-        image: "/images/projects/GLADIATOR.png",
-        tag: ["Todos", "Movil"],
-        gitUrl: "https://github.com/DiegoHS07/App_Gladiator/",
-        skills:[
-            {
-                name: "GIT",
-                icon: "git.png",
-            },
-            {
-                name: "KOTLIN",
-                icon: "kotlin.png",
-            },
-            {
-                name: "FIREBASE",
-                icon: "firebase.png",
-            },
-        ],
-    },
+  {
+    id: 1,
+    title: "Portafolio web con NextJs",
+    description:
+      "Portafolio virtual donde puedo mostrar mi cv de forma innovadora",
+    image: "/images/projects/Portafolio-web.png",
+    tag: ["Todos", "Web"],
+    gitUrl: "https://github.com/DiegoHS07/portafolio-web",
+    skills: [
+      {
+        name: "GIT",
+        icon: "git.png",
+      },
+      {
+        name: "HTML",
+        icon: "html.png",
+      },
+      {
+        name: "CSS3",
+        icon: "css.png",
+      },
+      {
+        name: "JAVASCRIPT",
+        icon: "javascript.png",
+      },
+      {
+        name: "REACT",
+        icon: "react.png",
+      },
+      {
+        name: "TAILWIND",
+        icon: "tailwind.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Pokedex con Lit-Element",
+    description:
+      "Creación de pokedex demostrando mis habilidades al implementar API's",
+    image: "/images/projects/Pokedex-LitElement.png",
+    tag: ["Todos", "Web"],
+    gitUrl: "https://github.com/DiegoHS07/Pokedex-LitElement",
+    skills: [
+      {
+        name: "GIT",
+        icon: "git.png",
+      },
+      {
+        name: "HTML",
+        icon: "html.png",
+      },
+      {
+        name: "CSS3",
+        icon: "css.png",
+      },
+      {
+        name: "JAVASCRIPT",
+        icon: "javascript.png",
+      },
+      {
+        name: "LITELEMENT",
+        icon: "litelement.png",
+      },
+      {
+        name: "NPM",
+        icon: "npm.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "SAAM",
+    description:
+      "Creación proyecto fin de grado y presentado en ACOFI 2020 y 2021",
+    image: "/images/projects/SAAM.png",
+    tag: ["Todos", "Movil"],
+    gitUrl: "https://github.com/DiegoHS07/UCC_SAAM",
+    skills: [
+      {
+        name: "GIT",
+        icon: "git.png",
+      },
+      {
+        name: "DART",
+        icon: "dart.png",
+      },
+      {
+        name: "FLUTTER",
+        icon: "flutter.png",
+      },
+      {
+        name: "FIREBASE",
+        icon: "firebase.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Gladiadores APP",
+    description: "Creación videojuego RPG temática de gladiadores para Android",
+    image: "/images/projects/GLADIATOR.png",
+    tag: ["Todos", "Movil"],
+    gitUrl: "https://github.com/DiegoHS07/App_Gladiator/",
+    skills: [
+      {
+        name: "GIT",
+        icon: "git.png",
+      },
+      {
+        name: "KOTLIN",
+        icon: "kotlin.png",
+      },
+      {
+        name: "FIREBASE",
+        icon: "firebase.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "TETRIS WEB",
+    description: "Videojuego desarrollado con Vite",
+    image: "/images/projects/tetris.jpg",
+    tag: ["Todos", "Web"],
+    gitUrl: "https://github.com/DiegoHS07/tetris-project",
+    skills: [
+      {
+        name: "GIT",
+        icon: "git.png",
+      },
+      {
+        name: "HTML",
+        icon: "html.png",
+      },
+      {
+        name: "CSS",
+        icon: "css.png",
+      },
+      {
+        name: "Javascript",
+        icon: "javascript.png",
+      },
+      {
+        name: "Vite",
+        icon: "vite.png",
+      },
+    ],
+  },
 ];
--- src/app/data/skillsData.js
+++ src/app/data/skillsData.js
@@ -1,82 +1,86 @@
 export const skillsData = [
-    {
-        name: "GIT",
-        icon: "git.png",
-    },
-    {
-        name: "HTML",
-        icon: "html.png",
-    },
-    {
-        name: "CSS3",
-        icon: "css.png",
-    },
-    {
-        name: "JAVASCRIPT",
-        icon: "javascript.png",
-    },
-    {
-        name: "REACT.JS",
-        icon: "react.png",
-    },
-    {
-        name: "TAILWIND",
-        icon: "tailwind.png",
-    },
-    {
-        name: "TYPESCRIPT",
-        icon: "typescript.png",
-    },
-    {
-        name: "CELLS",
-        icon: "cells.png",
-    },
-    {
-        name: "PHP",
-        icon: "php.png",
-    },
-    {
-        name: "ASP .NET",
-        icon: "asp-net.png",
-    },
-    {
-        name: "AWS",
-        icon: "aws.png",
-    },
-    {
-        name: "DART",
-        icon: "dart.png",
-    },
-    {
-        name: "FLUTTER",
-        icon: "flutter.png",
-    },
-    {
-        name: "FIREBASE",
-        icon: "firebase.png",
-    },
-    {
-        name: "KOTLIN",
-        icon: "kotlin.png",
-    },
-    {
-        name: "LITELEMENT",
-        icon: "litelement.png",
-    },
-    {
-        name: "POLYMER 2",
-        icon: "polymer-2.png",
-    },
-    {
-        name: "MySQL",
-        icon: "mysql.png",
-    },
-    {
-        name: "NPM",
-        icon: "npm.png",
-    },
-    {
-        name: "SASS",
-        icon: "sass.png",
-    },
+  {
+    name: "GIT",
+    icon: "git.png",
+  },
+  {
+    name: "HTML",
+    icon: "html.png",
+  },
+  {
+    name: "CSS3",
+    icon: "css.png",
+  },
+  {
+    name: "JAVASCRIPT",
+    icon: "javascript.png",
+  },
+  {
+    name: "REACT.JS",
+    icon: "react.png",
+  },
+  {
+    name: "TAILWIND",
+    icon: "tailwind.png",
+  },
+  {
+    name: "TYPESCRIPT",
+    icon: "typescript.png",
+  },
+  {
+    name: "VITE.JS",
+    icon: "vite.png",
+  },
+  {
+    name: "CELLS",
+    icon: "cells.png",
+  },
+  {
+    name: "PHP",
+    icon: "php.png",
+  },
+  {
+    name: "ASP .NET",
+    icon: "asp-net.png",
+  },
+  {
+    name: "AWS",
+    icon: "aws.png",
+  },
+  {
+    name: "DART",
+    icon: "dart.png",
+  },
+  {
+    name: "FLUTTER",
+    icon: "flutter.png",
+  },
+  {
+    name: "FIREBASE",
+    icon: "firebase.png",
+  },
+  {
+    name: "KOTLIN",
+    icon: "kotlin.png",
+  },
+  {
+    name: "LITELEMENT",
+    icon: "litelement.png",
+  },
+  {
+    name: "POLYMER 2",
+    icon: "polymer-2.png",
+  },
+  {
+    name: "MySQL",
+    icon: "mysql.png",
+  },
+  {
+    name: "NPM",
+    icon: "npm.png",
+  },
+  {
+    name: "SASS",
+    icon: "sass.png",
+  },
 ];
